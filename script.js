f59a129296b1dd66c0ad8dab3b56590987f64629
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2024-03-13 18:16:31 +0530

    sixth commit

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ function postCard(img, title, para, id) {
   div.classList.add("post-recent");
   div.innerHTML = `
   <div class="post-img"><a href="/article.html#${id}">
-  <img src="${img}" alt="img" onerror="this.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiqNdH4rC5njlxoA0Iwru70QCslWJItVBH2Q&usqp=CAU'">
+  <img class="card-img" src="${img}" alt="img" onerror="this.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiqNdH4rC5njlxoA0Iwru70QCslWJItVBH2Q&usqp=CAU'">
   </a></div>
             <div class="post-heading">
-              <a href="/article.html#${id}"><h2>${title}</h2></a>
-              <p>${para}</p>
+              <a href="/article.html#${id}"><h2 class="card-heading">${title}</h2></a>
+              <p class="card-para">${para}</p>
             </div>
+            <button class="main-read-later" id=${id}>Read Later</button>
   `;
   return div;
 }
@@ -39,7 +40,52 @@ async function addHtml() {
 addHtml();
 
 // ADDING LOAD MORE CODE
+let heroSection = document.querySelector(".hero-section");
 loadBtn.addEventListener("click", () => {
   pageNum = pageNum + 1;
   addHtml();
+});
+
+// HERO SECTION ANIMATION
+let hero = document.querySelectorAll(".hero");
+let arrowRight = document.querySelector("#right-arrow");
+let arrowLeft = document.querySelector("#left-arrow");
+
+function addingInitial(value, index) {
+  value.style.transform = `translateX(${index * 100}%)`;
+}
+
+hero.forEach((value, index) => {
+  addingInitial(value, index);
+});
+
+arrowRight.addEventListener("click", function () {
+  hero[0].style.transform = `translateX(-100%)`;
+  hero[1].style.transform = `translateX(0%)`;
+});
+arrowLeft.addEventListener("click", function () {
+  hero[0].style.transform = `translateX(0%)`;
+  hero[1].style.transform = `translateX(100%)`;
+});
+
+// ADD READ LATER CODE
+
+function setInlocal(img, name, id) {
+  let localData = JSON.parse(localStorage.getItem("ids"));
+  if (!localData) {
+    localStorage.setItem("ids", JSON.stringify([{ img, name, id }]));
+  } else {
+    localData.push({ img, name, id });
+    localStorage.setItem("ids", JSON.stringify(localData));
+  }
+}
+
+document.querySelector("body").addEventListener("click", function (e) {
+  if (e.target.classList.contains("main-read-later")) {
+    let id = e.target.getAttribute("id");
+    let parent = e.target.closest("div");
+    let img = parent.querySelector(".card-img");
+    let heading = parent.querySelector(".card-heading");
+    setInlocal(img.src, heading.innerHTML, id);
+  }
 });
